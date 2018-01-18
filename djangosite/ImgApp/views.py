6c233863153a78c4cb26210275b43425b8775741
memuGie <just@me.com>
2018-01-18 20:50:19 +0100

    Save appropriate image info in the models Job done!
    TaskRef: https://github.com/memuGie/DjangoApp/issues/14

--- djangosite/ImgApp/views.py
+++ djangosite/ImgApp/views.py
@@ -57,15 +57,34 @@ def _query_image_info(img_url):
 
 def _save_image_info(photo, img_info):
     photo_info = json.loads(img_info)
+    pi = _save_photo_info(photo, photo_info)
+    _save_photo_faces(pi, photo_info['faces'])
+
+
+def _save_photo_info(photo, photo_info):
     pi = PhotoInfo.objects.create(
         caption=photo_info['description']['captions'][0]['text'],
         tags=str(photo_info['description']['tags']),
-        raw_json=img_info,
+        raw_json=json.dumps(photo_info),
         width=photo_info['metadata']['width'],
         height=photo_info['metadata']['height'],
         format=photo_info['metadata']['format'].lower(),
         photo_ref=photo)
     pi.save()
+    return pi
+
+
+def _save_photo_faces(photo_info, faces_list):
+    for face in faces_list:
+        f = Face.objects.create(
+            age=face['age'],
+            gender=face['gender'],
+            top=face['faceRectangle']['top'],
+            left=face['faceRectangle']['left'],
+            width=face['faceRectangle']['width'],
+            height=face['faceRectangle']['height'],
+            photo_info_ref=photo_info)
+        f.save()
 
 
 def _apply_face_rectangles_pillow(img_url, img_info):
