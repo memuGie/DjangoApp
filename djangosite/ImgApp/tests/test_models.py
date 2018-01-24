60c8e6bdd0e0645ee7abd7c95898fc10679c5a2b
memuGie <just@me.com>
2018-01-24 22:51:51 +0100

    Improved models UT

--- djangosite/ImgApp/tests/test_models.py
+++ djangosite/ImgApp/tests/test_models.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from django.utils import timezone
 from django.contrib.auth.models import User
 
-from ..models import Photo
+from ..models import Photo, PhotoInfo, Face
 
 
 class ModelTests(TestCase):
@@ -18,8 +18,61 @@ class ModelTests(TestCase):
             owner_ref=self._user,
             image=tempfile.NamedTemporaryFile(suffix=".jpg").name
         )
+        self._photo_info = PhotoInfo.objects.create(
+            caption="a photo taken on Mallorca",
+            tags="['mallorca', 'people', 'posing', 'couple']",
+            raw_json="{'test': 'test value'}",
+            width=500,
+            height=500,
+            format="jpg",
+            photo_ref=self._photo
+        )
+        self._face1 = Face.objects.create(
+            age=30,
+            gender="Male",
+            top=30,
+            left=30,
+            width=50,
+            height=50,
+            photo_info_ref=self._photo_info
+        )
+        self._face2 = Face.objects.create(
+            age=25,
+            gender="Female",
+            top=30,
+            left=70,
+            width=50,
+            height=50,
+            photo_info_ref=self._photo_info
+        )
+        self._photo.save()
 
-    def test_create_photo(self):
+    def test_photo_attributes(self):
         self.assertEqual(self._photo.description, "test photo from Mallorca")
         self.assertEqual(self._photo.owner_ref, self._user)
         self.assertTrue(self._photo.image.url)
+
+    def test_photo_photo_info(self):
+        self.assertEqual(self._photo.photoinfo, self._photo_info)
+
+    def test_photo_info_attributes(self):
+        self.assertEqual(self._photo_info.caption, "a photo taken on Mallorca")
+        self.assertEqual(self._photo_info.tags, "['mallorca', 'people', 'posing', 'couple']")
+        self.assertEqual(self._photo_info.raw_json, "{'test': 'test value'}")
+        self.assertEqual(self._photo_info.width, 500)
+        self.assertEqual(self._photo_info.height, 500)
+        self.assertEqual(self._photo_info.format, "jpg")
+        self.assertEqual(self._photo_info.photo_ref, self._photo)
+
+    def test_photo_info_faces(self):
+        self.assertEqual(self._photo_info.face_set.get(pk=1), self._face1)
+        self.assertEqual(self._photo_info.face_set.get(pk=2), self._face2)
+
+    def test_face_attributes(self):
+        self.assertEqual(self._face1.age, 30)
+        self.assertEqual(self._face1.gender, "Male")
+        self.assertEqual(self._face1.top, 30)
+        self.assertEqual(self._face1.left, 30)
+        self.assertEqual(self._face1.width, 50)
+        self.assertEqual(self._face1.height, 50)
+        self.assertEqual(self._face1.photo_info_ref, self._photo_info)
